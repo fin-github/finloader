9e62bf6d22163cb0b5fbbbb0516146bdb45314ea
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2023-12-13 19:03:24 -0500

    Create vars.py

---
+++ FULL/vars.py
@@ -0,0 +1,5 @@
+#fyi, you dont have to know py, dw
+# example = {"Tool1name": "Tool1Filename","Tool2name": "Tool2Filename","Tool3name": "Tool3Filename",}
+# you can copy this ^^^ and change the values lol.
+
+tools = {"examplename": "examplefilename"}
