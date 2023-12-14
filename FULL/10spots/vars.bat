06beef334153d2dfeed7973b08879f807489cbc7
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2023-12-13 19:15:09 -0500

    Create vars.bat

---
+++ FULL/10spots/vars.bat
@@ -0,0 +1,83 @@
+@ECHO OFF
+rem -------IGNORE
+if "%1" equ "" (
+    ECHO RUN MAIN.BAT TO START.
+    pause
+    exit
+)
+rem -------IGNORE
+
+
+
+
+REM ---------------------------TOOL NAMES---------------------------
+REM SET TOOL 1 NAME HERE
+SET t1name=-
+REM SET TOOL 2 NAME HERE
+SET t2name=-
+REM SET TOOL 3 NAME HERE
+SET t3name=-
+REM SET TOOL 4 NAME HERE
+SET t4name=-
+REM SET TOOL 5 NAME HERE
+SET t5name=-
+REM SET TOOL 6 NAME HERE
+SET t6name=-
+REM SET TOOL 7 NAME HERE
+SET t7name=-
+REM SET TOOL 8 NAME HERE
+SET t8name=-
+REM SET TOOL 9 NAME HERE
+SET t9name=-
+
+
+
+REM ----------------------TOOL PATHS--------------------------------------------
+REM RELATIVE PATH TO THE TOOL 1
+SET t1path=tools
+REM RELATIVE PATH TO THE TOOL 2
+SET t2path=tools
+REM RELATIVE PATH TO THE TOOL 3
+SET t3path=tools
+REM RELATIVE PATH TO THE TOOL 4
+SET t4path=tools
+REM RELATIVE PATH TO THE TOOL 5
+SET t5path=tools
+REM RELATIVE PATH TO THE TOOL 6
+SET t6path=tools
+REM RELATIVE PATH TO THE TOOL 7
+SET t7path=tools
+REM RELATIVE PATH TO THE TOOL 8
+SET t8path=tools
+REM RELATIVE PATH TO THE TOOL 9
+SET t9path=tools
+REM RELATIVE PATH TO THE TOOL 10
+SET t10path=tools
+
+
+REM ----------------------TOOL FILENAMES------------------------------------------------------------
+REM NAME OF TOOL 1 FILE
+SET t1filename=null.bat
+REM NAME OF TOOL 2 FILE
+SET t2filename=null.bat
+REM NAME OF TOOL 3 FILE
+SET t3filename=null.bat
+REM NAME OF TOOL 4 FILE
+SET t4filename=null.bat
+REM NAME OF TOOL 5 FILE
+SET t5filename=null.bat
+REM NAME OF TOOL 6 FILE
+SET t6filename=null.bat
+REM NAME OF TOOL 7 FILE
+SET t7filename=null.bat
+REM NAME OF TOOL 8 FILE
+SET t8filename=null.bat
+REM NAME OF TOOL 9 FILE
+SET t9filename=null.bat
+REM NAME OF TOOL 107 FILE
+SET t10filename=null.bat
+
+
+REM ----------------------OTHER SETTINGS--------------------------
+REM AFTER MULTITOOL IS COMPLETE: 1: GOTO VERY START 2: GO TO MENU START 3: PAUSE, THEN QUIT 4: INSTANT QUIT (NOT RECCOMENDED)
+SET postmultitool=1
